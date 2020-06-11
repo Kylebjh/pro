d32faedde2fe71965ddbe59686721dfe2dc2c07a
root <ahalwl11@naver.com>
2020-06-11 16:00:08 +0900

    keyboard_read함수 추가

--- src/keypad.c
+++ src/keypad.c
@@ -1,10 +1,40 @@
 #include "keypad.h"
+#include "stdio.h"//scanf
+#include "string.h"//strlen
 
 static short * keypad_out, *keypad_in;
+
+char keyboard[16] = {'1', '2', '3', '4',
+	    	     'q', 'w', 'e', 'r',
+		     'a', 's', 'd', 'f',
+		     'z', 'x', 'c', 'v'};
 
 void init_keypad(short * address_out, short * address_in){
 	keypad_out = address_out;
 	keypad_in = address_in;
+}
+
+int keyboard_read(int * key_value){
+	int key_count =0;
+	char buf[10];
+	scanf("%s", buf)
+	key_count = strlen(buf);
+
+	//printf("key_count : %d\n",key_count);
+
+	if (key_count == 0){
+		return key_count;
+	}
+	else {
+		for(int i=0; i < key_count ; i++){
+			for(j = 0 ; j < 16 < j++){
+				if(buf[i] == keyboard[j]){
+					*key_value = j;
+				}
+			}
+		}
+		return key_count;
+	} 
 }
 
 int keypad_read(int * key_value) {
